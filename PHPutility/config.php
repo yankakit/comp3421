079542d56cd1ddbb4d57449b264d0c2bd26f9793
yankakit <102674350+yankakit@users.noreply.github.com>
2023-04-14 22:38:11 +0800

    Update config.php

--- PHPutility/config.php
+++ PHPutility/config.php
@@ -10,7 +10,6 @@ define('DB_NAME', 'heroku_66bb85761ed9b1b');
 //define('DB_PASSWORD', '98717b74');
 //define('DB_NAME', 'heroku_bdd8b614aa3e447');
 /* Attempt to connect to MySQL database */
-mysql://b5a3f3423c0f95:448d8a45@us-cdbr-east-06.cleardb.net/heroku_66bb85761ed9b1b?reconnect=true
 try{
     $db = new PDO("mysql:host=" . DB_SERVER . ";dbname=" . DB_NAME, DB_USERNAME, DB_PASSWORD);
     // Set the PDO error mode to exception
